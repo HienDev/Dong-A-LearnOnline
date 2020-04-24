38271025f7a30d556a3e8adbaa4e6d9f804bb347
Hoang Duc Hien <hoangduchien200@gmail.com>
2020-04-24 13:29:58 +0700

    Add project files.

---
+++ Dong A LearnOnline/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Dong_A_LearnOnline.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
