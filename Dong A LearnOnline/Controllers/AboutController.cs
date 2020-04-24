7d0ec1d79f425318086b33ada14b49e5b32fbe66
Hoang Duc Hien <hoangduchien200@gmail.com>
2020-04-24 13:33:51 +0700

    Create a AboutController

---
+++ Dong A LearnOnline/Controllers/AboutController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dong_A_LearnOnline.Controllers
+{
+    public class AboutController : Controller
+    {
+        public string Index()
+        {
+            return "Hello World";
+        }
+    }
+}
